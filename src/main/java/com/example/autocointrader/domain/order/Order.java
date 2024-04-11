004e95a0146f3a23e5f6fd849b148c6c5cb6c58a
jay <joswxx@gmail.com>
2024-04-11 22:06:33 +0900

    .

--- src/main/java/com/example/autocointrader/domain/order/Order.java
+++ src/main/java/com/example/autocointrader/domain/order/Order.java
@@ -14,6 +14,34 @@ public class Order {
     private Optional<String> volume;
 
 
+    private Order(Side side, OrderType orderType, String market, Optional<Double> price, Optional<String> volume) {
+
+        this.side = side;
+        this.orderType = orderType;
+        this.market = market;
+        this.price = price;
+        this.volume = volume;
+
+    }
+
+//    만약에 , 지정가까지 구현을한다면 두개타입이 넘어와서 아래와같이 구분을 해줘야하지만 , 당장은 필요가없다 .
+
+//    public boolean isMarketOrder() {
+//        return orderType == OrderType.MARKET;
+//    }
+//
+//    public boolean isLimitOrder() {
+//        return orderType == OrderType.LIMIT;
+//    }
+
+    public static Order createForMarketBid(String market, Optional<Double> price) {
+
+        return new Order(Side.BID, OrderType.MARKET, market, price, null);
+
+    }
+
+
+
 
 
 }
